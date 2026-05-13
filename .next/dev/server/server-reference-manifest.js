e1de536db0b9735e2bde6052941dd7e8535470d9
Your Name <your.email@example.com>
2026-05-13 22:16:09 +0700

    ignore build files

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"d+h719sfeBA4R3Wznv1F+g+yBUhglfzTFw0HQgi42VA=\"\n}"
--- .next/dev/static/chunks/app_page_tsx_01oyzok._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_pdf-lib_es_core_06od42-._.js","static/chunks/node_modules_pdf-lib_es_api_0-eis6b._.js","static/chunks/node_modules_pdf-lib_es_0.7misl._.js","static/chunks/node_modules_pako_05sbe~5._.js","static/chunks/node_modules_@pdf-lib_standard-fonts_es_019jv7z._.js","static/chunks/node_modules_0.~m207._.js","static/chunks/app_page_tsx_0~3a212._.js"],
-    source: "dynamic"
-});
